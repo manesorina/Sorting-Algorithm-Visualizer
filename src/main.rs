83fa2e3cb373de42941a81c58612f9f9dc8ce21c
so17 <manesorina@gmail.com>
2025-03-18 20:48:18 +0200

    Fixed animation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{thread, time::Duration};
 struct SortVisualizer {
     numbers: Vec<i32>,
     is_sorting: bool,
+    rx: Option<std::sync::mpsc::Receiver<Vec<i32>>>,
 }
 
 impl Default for SortVisualizer {
@@ -12,6 +13,7 @@ impl Default for SortVisualizer {
         Self {
             numbers: (0..50).map(|_| rand::thread_rng().gen_range(10..400)).collect(),
             is_sorting: false,
+            rx: None,
         }
     }
 }
@@ -20,23 +22,31 @@ impl SortVisualizer {
     fn bubble_sort(&mut self, ctx: &egui::Context) {
         self.is_sorting = true;
         let len = self.numbers.len();
-
+        
+        let (tx, rx) = std::sync::mpsc::channel();
+        
         let mut numbers_clone = self.numbers.clone();
         let ctx_clone = ctx.clone();
-
+        
         thread::spawn(move || {
             for i in 0..len {
                 for j in 0..len - i - 1 {
                     if numbers_clone[j] > numbers_clone[j + 1] {
                         numbers_clone.swap(j, j + 1);
+                        
+                        
+                        tx.send(numbers_clone.clone()).unwrap();
+                        
+                        thread::sleep(Duration::from_millis(30));
+                        ctx_clone.request_repaint();
                     }
-                    thread::sleep(Duration::from_millis(30)); 
-                    ctx_clone.request_repaint(); 
                 }
             }
         });
+        
+        
+        self.rx = Some(rx);
     }
-
     fn reset_array(&mut self) {
         self.numbers = (0..50).map(|_| rand::thread_rng().gen_range(10..400)).collect();
         self.is_sorting = false;
@@ -45,6 +55,15 @@ impl SortVisualizer {
 
 impl eframe::App for SortVisualizer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+        if let Some(rx) = &self.rx {
+            if let Ok(new_numbers) = rx.try_recv() {
+                self.numbers = new_numbers;
+            }
+        }
+
+
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 if ui.button("Start Sorting").clicked() && !self.is_sorting {
